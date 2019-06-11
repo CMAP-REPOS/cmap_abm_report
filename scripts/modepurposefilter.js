e760c105c37486ba0b219dde09fd4f19b476af6f
brittaneyross <brittaney.e.ross@gmail.com>
2019-06-11 10:19:38 -0500

    updating auto ownership

--- scripts/modepurposefilter.js
+++ scripts/modepurposefilter.js
@@ -1,10 +1,10 @@
 function make_h_stacked_tripsbymode(csv_file,divID, legendID){
     var margin = {top: 80, right: 10, bottom: 0, left: 75},
-      width = 700 - margin.left - margin.right,
+      width = 900 - margin.left - margin.right,
       height = 300 - margin.top - margin.bottom;
-  
-  
-  
+
+
+
     var g = d3.select("#"+divID).append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -21,23 +21,23 @@ function make_h_stacked_tripsbymode(csv_file,divID, legendID){
       Walk_to_transit: 'Transit',
       School_bus: 'HOV',
       Bike: 'Non-motorized',
-      Drive_alone_pay: 'SOV',	
-      Drive_to_transit: 'Transit',	
-      Shared_ride_2plus_pay: 'HOV',	
+      Drive_alone_pay: 'SOV',
+      Drive_to_transit: 'Transit',
+      Shared_ride_2plus_pay: 'HOV',
       Taxi: 'HOV'
     }
-  
+
       var y = d3.scaleBand()
           .rangeRound([0, height])
           .padding(0.1)
           .align(0.1);
-  
+
       var x = d3.scaleLinear()
           .rangeRound([width, 0]);
-  
+
       var z = d3.scaleOrdinal()
           .range(['#5cbddd','#1c5f83','#66666E','#fefefe','#7ebea5','#3c765f','#EFE9AE','#AFBE8F']);
-  
+
       var stack = d3.stack()
           .offset(d3.stackOffsetExpand);
 
@@ -47,27 +47,27 @@ function make_h_stacked_tripsbymode(csv_file,divID, legendID){
 
       d3.csv(csv_file, type, function (error, data) {
           if (error) throw error;
-  
+
           // List of subgroups = header of the csv files = soil condition here
           var subgroups = data.columns.slice(1)
-  
+
           // List of groups = species here = value of the first column called group -> I show them on the X axis
           var groups = d3.map(data, function(d){return(d.Index)}).keys()
-  
+
           y.domain(data.map(function (d) {
               return d.Index;
           }));
           z.domain(data.columns.slice(1));
-  
+
           var mouseover = function(d, i) {
             // what subgroup are we hovering?
             var subgroupName = d3.select(this.parentNode).datum().key; // This was the tricky part
             var subgroupValue = d.data[subgroupName];
             var obsvalue;
             var modelvalue;
-  
+
             d3.selectAll("."+divID+"class").style("opacity", 0.2).select("text").remove();
-  
+
             var bardata = serie.selectAll("rect")
                 .data(function (d) {
                   if(d.key == subgroupName){
@@ -77,27 +77,27 @@ function make_h_stacked_tripsbymode(csv_file,divID, legendID){
                   }
                   return d
                 })
-  
+
               //console.log(obsvalue)
               div.transition()
               .duration(200)
               .style("opacity", .9);
               div.html(
-                typeobj[subgroupName] + 
-                "<br><b><p style='font-size: 12px'; color: grey'>" + subgroupName.replace(/_/g, " ") + 
-                "</p></b><p style='color:rgb(28, 78, 128); font-size: 20px; margin-bottom: 0px;'>" + d3.format(".4~s")(obsvalue) + 
+                typeobj[subgroupName] +
+                "<br><b><p style='font-size: 12px'; color: grey'>" + subgroupName.replace(/_/g, " ") +
+                "</p></b><p style='color:rgb(28, 78, 128); font-size: 20px; margin-bottom: 0px;'>" + d3.format(".4~s")(obsvalue) +
                 "</p><p style='color:grey; font-size: 10px;'> modeled" +
                 "</p><p style='color:rgb(166, 186, 206); font-size: 20px; margin-bottom: 0px;'>" + d3.format(".4~s")(modelvalue) +
-                "</p><p style='color:grey; font-size: 10px;'> observed </p>" 
+                "</p><p style='color:grey; font-size: 10px;'> observed </p>"
                 )
                 .style("left", (d3.event.pageX - 300) + "px")
                 .style("top", (d3.event.pageY - 350) + "px");
-    
+
               // Highlight all rects of this subgroup with opacity 0.8. It is possible to select them since they have a specific class = their name.
               d3.selectAll("."+ subgroupName)
                 .style("opacity", 1)
             }
-    
+
         // When user do not hover anymore
         var mouseleave = function(d) {
           // Back to normal opacity: 0.8
@@ -105,7 +105,7 @@ function make_h_stacked_tripsbymode(csv_file,divID, legendID){
             .style("opacity",0.8)
             .select("text").remove();
           }
-    
+
         var serie = g.selectAll(".serie")
             .data(stack.keys(data.columns.slice(1))(data))
             .enter().append("g")
@@ -143,15 +143,15 @@ function make_h_stacked_tripsbymode(csv_file,divID, legendID){
       var legend = d3.select("#"+legendID).append("svg")
       .attr("height", 300)
       .attr("width", 250)
-  
+
       // create a list of keys
       var keys = data.columns.slice(1)
-  
+
       // Usually you have a color scale in your chart already
       var color = d3.scaleOrdinal()
         .domain(keys)
         .range(['#5cbddd','#1c5f83','#66666E','#fefefe','#7ebea5','#3c765f','#EFE9AE','#AFBE8F']);
-  
+
       // Add one dot in the legend for each name.
       legend.selectAll("mydots")
         .data(keys)
@@ -161,7 +161,7 @@ function make_h_stacked_tripsbymode(csv_file,divID, legendID){
           .attr("cy", function(d,i){ return 20 + i*25}) // 100 is where the first dot appears. 25 is the distance between dots
           .attr("r", 7)
           .style("fill", function(d){ return color(d)})
-  
+
       // Add one dot in the legend for each name.
       legend.selectAll("mylabels")
         .data(keys)
@@ -173,7 +173,7 @@ function make_h_stacked_tripsbymode(csv_file,divID, legendID){
           .attr("text-anchor", "left")
           .style("alignment-baseline", "middle")
           });
-  
+
       function type(d, i, columns) {
           var t;
           for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
@@ -183,5 +183,3 @@ function make_h_stacked_tripsbymode(csv_file,divID, legendID){
 
 
   }
-  
-

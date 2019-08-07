545f4e3733c516aad95b7cae018c431acf174d8a
sarahcmap <sbuchhorn@cmap.illinois.gov>
2019-08-07 17:00:26 -0500

    logo

--- scripts/h_stacked.js
+++ scripts/h_stacked.js
@@ -79,8 +79,8 @@ function make_h_stacked(csv_file,divID, legendID){
             "</p><p style='color:rgb(166, 186, 206); font-size: 20px; margin-bottom: 0px;'>" + d3.format(".4~s")(obsvalue) +
             "</p><p style='color:grey; font-size: 10px;'> observed </p>"
             )
-            .style("left", (x + 100) + "px")
-            .style("top",  (y + 100) + "px");
+            .style("left", (x + 50) + "px")
+            .style("top",  (y + 70) + "px");
 
           // Highlight all rects of this subgroup with opacity 0.8. It is possible to select them since they have a specific class = their name.
           d3.selectAll("."+subgroupName)
